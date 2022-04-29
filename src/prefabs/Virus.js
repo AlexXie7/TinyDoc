7ff9f2da69bf3797ccbdcc7c94bd51052ec99d75
AlexXie7 <103275372+AlexXie7@users.noreply.github.com>
2022-04-29 10:18:51 -0700

    virus splits

--- src/prefabs/Virus.js
+++ src/prefabs/Virus.js
@@ -1,6 +1,20 @@
 class Virus extends Enemy {
-    constructor(scene, x, y, texture, frame, pointValue){
+    constructor(scene, x, y, texture, frame, pointValue, scale = .3){
         super(scene, x, y, texture, frame, pointValue);
-        this.setScale(.1);
+        this.scale = scale;
+        this.setScale(scale);
     }    
+
+
+    destroy(){
+        if(this.scale > 0.1 && !this.isDestroyed){
+            var newScale = this.scale/3;
+            if (newScale < 0.1){
+                newScale = 0.1;
+            }
+            this.scene.enemies.push(new Virus(this.scene, this.x, this.y, 'EnemyGreen', 0, 30, newScale));
+            this.isDestroyed = true;
+        }
+        super.destroy();
+    }
 }
