6cc09ed13ae7a10e150dae59bfd7bcdff1ff781c
AlexXie7 <103275372+AlexXie7@users.noreply.github.com>
2022-05-02 18:49:18 -0700

    Update Menu.js

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -21,8 +21,8 @@ class Menu extends Phaser.Scene {
         let menuConfig = {
             fontFamily: 'Courier',
             fontSize: '64px',
-            backgroundColor: '#F3B141',
-            color: '#CC2006',
+            //backgroundColor: '#F3B141',
+            color: '#FFFFFF',
             align: 'right',
             padding: {
                 top: 5,
@@ -62,7 +62,7 @@ class Menu extends Phaser.Scene {
         wiggle2.anims.play('menuWiggle');
 
         //Medicine Legend
-        let legend = this.add.text(game.config.width/9, game.config.height/8*7, 'Use these medicines to cure these enemies', menuConfig);
+        let legend = this.add.text(game.config.width/10, game.config.height/8*7, 'Use these medicines to cure these enemies:', menuConfig);
         let legendOrangeEnemy = this.add.sprite(legend.x + legend.width, legend.y + legend.height, 'EnemyOrange').setScale(0.3).setOrigin(0);
         this.add.sprite(legendOrangeEnemy.x, legendOrangeEnemy.y, 'medicine1').setOrigin(0, 1);
 
